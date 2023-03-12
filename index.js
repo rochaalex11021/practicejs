8f71e9f120165404342758d0973065f2f38631f4
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-11 18:41:37 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let person = {
+  name: "Alex",
+  age: 28,
+  country: "Mexico",
+};
+
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+
+logData();
