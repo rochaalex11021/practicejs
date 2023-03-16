02bf39780a2c24ee640dfb207cb838fa0abea0c2
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-16 14:46:37 -0500

    .

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function renderLeads() {
   let listItems = " ";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
-    <li>
-    <a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]}
-    </a>
-    </li>`;
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`;
 
     console.log(listItems);
   }
